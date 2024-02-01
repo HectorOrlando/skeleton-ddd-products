3f6660a096b75de76ecc873ca3e3cc98761a6deb
HectorOrlando <14orgui@gmail.com>
2024-02-01 17:50:25 +0100

    refactor ObjectId

--- src/shop/products/domain/ProductId.ts
+++ src/shop/products/domain/ProductId.ts
@@ -3,6 +3,9 @@
 import { ObjectId } from "../../shared/domain/ObjectId";
 
 export class ProductId extends ObjectId {
+    constructor(id: string) {
+        super(id);
+    }
 
     // Método estático para generar un ProductId aleatorio
     public static random(): ProductId {
--- src/shop/shared/domain/ObjectId.ts
+++ src/shop/shared/domain/ObjectId.ts
@@ -2,21 +2,21 @@
 
 import { ObjectId as MongoObjectID } from 'mongodb';
 
-export class ObjectId {
+export abstract class ObjectId {
     public readonly value: string;
 
-    public constructor(id: string) {
+    protected constructor(id: string) {
         this.checkIsValid(id);
         this.value = new MongoObjectID(id).toHexString();
     }
 
     public static random(): ObjectId {
         const id = new MongoObjectID().toHexString();
-        return new ObjectId(id);
+        return new (this as any)(id);
     }
 
     protected checkIsValid(value: string): void {
-        if(!MongoObjectID.isValid(value)) {
+        if (!MongoObjectID.isValid(value)) {
             throw new Error(`Id is not valid. ${value}`);
         }
     }
